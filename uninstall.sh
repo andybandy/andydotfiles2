41e89c82c3fbec1b8887aaa8ea094db24122fa5a
Andrei Shchatko <andrei.shchatko@gmail.com>
2013-12-28 09:01:15 +0400

    Fixes logging errors

--- uninstall.sh
+++ uninstall.sh
@@ -6,9 +6,9 @@ files="bashrc vimrc vim zshrc oh-my-zsh"    # list of files/folders to symlink i
 echo "Uninstalling andydotfiles2"
 
 for file in $files do
-  echo "Deleting symlinks to $file in home directory"
+  echo -n "Deleting symlinks to $file in home directory"
   rm -rf ~/.$file
-  echo "Moving dotfiles from $olddir to ~"
+  echo -n "Moving dotfiles from $olddir to ~"
   mv $olddir/$file ~/.$file
 done
 
